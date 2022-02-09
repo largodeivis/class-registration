3f79f1a7575fb8f526fcb9a3b2329e4a10d52577
Deivis Largo <dlargo67@gmail.com>
2022-02-09 10:51:36 -0600

    First commit

---
+++ src/main/java/com/academic/classregistration/ClassregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.academic.classregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academic/classregistration/ClassregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic.classregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
